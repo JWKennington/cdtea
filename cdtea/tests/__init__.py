56318133fc87563d007ce5ce70b501cda0859c92
JWKennington <jameswkennington@gmail.com>
2021-04-19 00:00:51 -0400

    add initial test suite stub

---
+++ cdtea/tests/__init__.py
@@ -0,0 +1,12 @@
+import pathlib
+import pytest
+
+TEST_ROOT = pathlib.Path(__file__).parent.parent
+
+
+def run_tests():
+    """Helper function for invoking pytest suite
+    Returns:
+        None
+    """
+    return pytest.main(['-x', TEST_ROOT.as_posix()])
