ccff7d8cc7fbd379834ade126ef115c9d306b1b3
Jackson Henry <jackson.henry3@gmail.com>
2021-04-26 09:37:06 -0400

    fix index argument order, add type hints, add better docstrings.

--- cdtea/generate_flat.py
+++ cdtea/generate_flat.py
@@ -1,37 +1,56 @@
 from cdtea import simplicial
+from typing import Union, FrozenSet, Set
 import random
 
+def generate_flat_2d_space_time(time_size: int, space_size: int) -> simplicial.Triangulation:
+    """
+    returns a flat 2d simplicial.Triangulation with toroidal topology consisting of
+    n 0-simplices, 3n 1-simplices, and 2n 2-simplices
+    where n = time_size*space_size.
 
-def generate_flat_2d_space_time(T, X):
-    st = simplicial.Triangulation()
+    Each simplex is instantiated with a "type" metadata with the convention being
+    type = [number of constituent 0-simplices in past, number of constituent 0-simplices in future]
+    0-simplices are not given a type because they all have the same type.
+    0-simplices are instantiated with a "t" metadata value which is a time index.
+    2-simplices are instantiated with a "dilaton" metadata value, which is random.
 
-    def add_simplex(basis, **meta):
-        st.add_simplex(simplicial.DimDSimplexKey(basis=basis), **meta)
+    time_size: an int specifying the number of space-like slices in the triangulation
+    space_size: an int specifying the resolution of each space-like slice
 
-    def idx(x, t):
-        # returns an index in the spacetime, corrects for overflow in space and time
-        return (t % T) * X + x % X
+    """
+    space_time = simplicial.Triangulation()
 
-    for t in range(T):
-        for x in range(X):
+    def add_simplex(basis: Union[Set, FrozenSet], **meta):
+        """shorthand for adding a simplex to the triangulation"""
+        space_time.add_simplex(simplicial.DimDSimplexKey(basis=basis), **meta)
+
+    def idx(x_idx: int, t_idx: int):
+        """shorthand for returning a unique index for a given space-time position.
+        It autocorrects for space and time overflow
+        """
+        return (t_idx % time_size) * space_size + x_idx % space_size
+
+    for t in range(time_size):
+        for x in range(space_size):
             # new vertex (one added per iteration )
-            i = idx(x, t)
-            add_simplex({i})
+            i = idx(x_idx=x, t_idx=t)
+            add_simplex({i}, t=t)
 
             # new edges (three added per vertex)
-            spatial_edge_basis = {idx(x, t), idx(t, x + 1)}
+            spatial_edge_basis = {idx(x_idx=x, t_idx=t), idx(x_idx=x + 1, t_idx=t)}
             add_simplex(spatial_edge_basis, type=[2, 0])
 
-            past_edge_basis = {idx(x, t), idx(t + 1, x)}
+            past_edge_basis = {idx(x_idx=x, t_idx=t), idx(x_idx=x, t_idx=t + 1)}
             add_simplex(past_edge_basis, type=[1, 1])
 
-            future_edge_basis = {idx(x, t), idx(t - 1, x - 1)}
+            future_edge_basis = {idx(x_idx=x, t_idx=t), idx(x_idx=x - 1, t_idx=t - 1)}
             add_simplex(future_edge_basis, type=[1, 1])
 
             # new triangles (two added per vertex)
-            up_triangle_basis = {idx(x, t), idx(x + 1, t), idx(x + 0, t + 1)}
+            # each triangle is instantiated with a random dilaton value between zero and 1 using random.Random()
+            up_triangle_basis = {idx(x_idx=x, t_idx=t), idx(x_idx=x + 1, t_idx=t), idx(x_idx=x + 0, t_idx=t + 1)}
             add_simplex(up_triangle_basis, type=[2, 1], dilaton=random.Random())
 
-            down_triangle_basis = {idx(x, t), idx(x + 1, t), idx(x + 1, t - 1)}
+            down_triangle_basis = {idx(x_idx=x, t_idx=t), idx(x_idx=x + 1, t_idx=t), idx(x_idx=x + 1, t_idx=t - 1)}
             add_simplex(down_triangle_basis, type=[1, 2], dilaton=random.Random())
-    return st
+    return space_time
--- cdtea/tests/test_generate_flat.py
+++ cdtea/tests/test_generate_flat.py
@@ -7,10 +7,9 @@ class TestGenerateFlat:
     def test_create(self):
         """This test makes sure that generate_flat creates a triangulation
         """
-        t = generate_flat.generate_flat_2d_space_time(3, 3)
+        t = generate_flat.generate_flat_2d_space_time(time_size=3, space_size=3)
 
         assert isinstance(t, simplicial.Triangulation)
-
 
     # additional test ideas. Waiting to implement these until more tools for accessing simplex data have been constructed.
     # each vertex has 6 adjacent edges
