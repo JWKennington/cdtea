3bf8000bbd3b01e60c8886d658c357a414b982ea
Jackson Henry <jackson.henry3@gmail.com>
2021-05-11 09:57:37 -0400

    remove IOSDevScript.py and add it to the .gitignore

--- IOSDevScript.py
+++
@@ -1,6 +0,0 @@
-import cdtea.generate_flat
-import cdtea.tests.valid_triangulation as validity
-from cdtea.simplicial import DimDSimplexKey
-st = cdtea.generate_flat.generate_flat_2d_space_time(23,23)
-print(validity.edges_dont_cross_time_slices(st))
-print(validity.is_valid(st))
