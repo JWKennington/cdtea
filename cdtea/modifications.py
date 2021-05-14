f7d96e8c989b8102645e3ae64b308bd669995404
Jackson Henry <jackson.henry3@gmail.com>
2021-05-13 20:38:57 -0400

    add an increase move to modifications

--- cdtea/modifications.py
+++ cdtea/modifications.py
@@ -41,7 +41,59 @@ def parity_move(triangulation: Triangulation, k1: SimplexKey, k2: SimplexKey):
         raise Exception("Faces are not neighbors")
 
 
+def increase_move(triangulation: Triangulation, k1: SimplexKey, k2: SimplexKey):
+    overlap = k1 & k2
+    union = k1 | k2
+    non_overlap = union - overlap
+    # print(union)
+    if overlap in triangulation.simplices[1]:
+        s_type = triangulation.simplex_meta[overlap]["s_type"]
+        if s_type == (2, 0):
+            meta = triangulation.simplex_meta
+            # This is where we modify the triangulation
+            # The dim 0 simplices remain unchanged.
+            top = k1 - overlap
+            bottom = k2 - overlap
+            overlap_list = overlap.basis_list
+            left = overlap_list[0]
+            right = overlap_list[1]
+
+            new_node = simplex_key(triangulation.max_index)
+
+            triangulation.add_simplex(new_node, t=meta[right]['t'])
+
+            # the dim 1 simplices. the spatial edge is removed and replaced with another
+            triangulation.remove_simplex(overlap)
+
+            triangulation.add_simplex(top | new_node, s_type=(1, 1))
+            triangulation.add_simplex(bottom | new_node, s_type=(1, 1))
+
+            triangulation.add_simplex(left | new_node, s_type=(2, 0))
+            triangulation.add_simplex(right | new_node, s_type=(2, 0))
+
+            # the dim 2 simplices, the two associated with the overlap are removed and the two new ones added
+            k1_s_type = meta[k1]["s_type"]
+            k2_s_type = meta[k2]["s_type"]
+
+            triangulation.add_simplex(top | new_node | left, s_type=k1_s_type)
+            triangulation.add_simplex(top | new_node | right, s_type=k1_s_type)
+
+            triangulation.add_simplex(bottom | new_node | left, s_type=k2_s_type)
+            triangulation.add_simplex(bottom | new_node | right, s_type=k2_s_type)
+
+            triangulation.remove_simplex(k1)
+            triangulation.remove_simplex(k2)
+
+
+        else:
+            raise Exception("Faces are spatial neighbors not temporal neighbors")
+    else:
+        raise Exception("Faces are not neighbors")
+
+
 key1 = simplex_key({1, 21, 22})
 key2 = simplex_key({1, 2, 22})
-parity_move(tri, key1, key2)
+key1 = simplex_key({10, 6, 5})
+key2 = simplex_key({1, 6, 5})
+increase_move(tri, key1, key2)
 validity.is_valid(tri)
