1c332d84ad975e03bc38104bcbb0f74b50a05abc
Jackson Henry <jackson.henry3@gmail.com>
2021-05-15 11:18:55 -0400

    towards coords

--- cdtea/Visualization/coordinates.py
+++ cdtea/Visualization/coordinates.py
@@ -2,44 +2,37 @@ from cdtea.generate_flat import generate_flat_2d_space_time
 from matplotlib import pyplot as plt
 from cdtea import simplicial
 from cdtea.simplicial import simplex_key
+from collections import defaultdict
 
-tri = generate_flat_2d_space_time(space_size=10, time_size=10)
-meta = tri.simplex_meta
-slab_height = .1
-
-origin = tri.simplices[0]
+st = generate_flat_2d_space_time(space_size=10, time_size=10)
+meta = st.simplex_meta
 
 
-def x_path(tri: simplicial.Triangulation, v1: simplicial.Dim0SimplexKey, v2: simplicial.Dim0SimplexKey, possible_edges,
-           used_edges=set()
-
-           ):
-    meta = tri.simplex_meta
-
-    t = meta[v1]['t']
-    if t != meta[v2]['t']:
-        raise Exception("Not on the same time slice")
-    else:
-        if v1 == v2:
-            return used_edges
-        edges = possible_edges
-
-        v = v1
-        for e in edges - used_edges:
-            new_v = e - v1
-            if new_v.dim == 0:
-                # print(new_v)
-                return x_path(tri, new_v, v2, used_edges={e} | used_edges, possible_edges=possible_edges)
+def get_t(face, meta):
+    a = set()
+    for b in face:
+        a.add(meta[b]['t'])
+    return tuple(a)
 
 
-possible_edges = {}  # get all spatial edges
-for e in tri.simplices[1]:
-    basis = e.basis_list
-    if meta[e]["s_type"] == (2, 0):
-        if meta[basis[0]]['t'] == t and meta[basis[1]]['t'] == t:
-            possible_edges.add(e)
+def ordering(origin, layer):
+    boundary = origin
+    not_indexed = layer - {origin}
+    index = {origin: 0}
+    for i in range(len(layer)):
+        for f in not_indexed:
+            if (f & boundary).dim == 1:
+                not_indexed -= {f}
+                index[f] = index[boundary] + 1
+                boundary = f
+                break
+
+    return index
 
 
+layers = defaultdict(list)
+for tri in st.simplices[2]:
+    layers[get_t(tri, meta)].append(tri)
 
-
-x_path(tri, simplex_key(1), simplex_key(5), possible_edges)
+#layers = {[tri for tri in st.simplices[2] if get_t(tri, meta)]}
+print(layers)
