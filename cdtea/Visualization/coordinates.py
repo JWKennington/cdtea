f418dc02769ee101595b9a1d5f5b10219e445f00
Jackson Henry <jackson.henry3@gmail.com>
2021-06-24 10:05:02 -0400

    get coordinates used in visualization

--- cdtea/Visualization/coordinates.py
+++ cdtea/Visualization/coordinates.py
@@ -1,101 +1,51 @@
-from cdtea.generate_flat import generate_flat_2d_space_time
-from matplotlib import pyplot as plt
-from cdtea import simplicial
-from cdtea.simplicial import simplex_key
-from collections import defaultdict
-
-st = generate_flat_2d_space_time(space_size=5, time_size=5)
-meta = st.simplex_meta
+from cdtea.Visualization.SpatialOrdering import spatial_ordering
+from cdtea.simplicial import Triangulation
+import numpy as np
 
 
-def get_t(face):
-    a = set()
-    for b in face:
-        a.add(meta[b]['t'])
-    return tuple(sorted(list(a)))
+def toroidal_coordinates(st: Triangulation):
+    meta = st.simplex_meta
+    nodes = st.simplices[0]
+    edges = st.simplices[1]
+    T = st.time_size
+    layers = {t: [n for n in nodes if meta[n]['t'] == t] for t in range(T)}
+    base_order = spatial_ordering(st, layers[0])
+    prev_order = base_order[0:2]
+    total_order = [base_order]
+    for t in range(1, T):
+        layer = layers[t]
+        L = len(layer)
+        for i in range(len(layer)):
+            if ((layer[i] | prev_order[0]) in edges) and ((layer[i] | prev_order[1]) in edges):
+                middle = layer[i]
+            elif (layer[i] | prev_order[1]) in edges:
+                right = layer[i]
+            elif (layer[i] | prev_order[0]) in edges:
+                left = layer[i]
+        if t % 2 == 0:
+            ORD = spatial_ordering(st, layers[t], indexed=[middle, right])
+        else:
+            ORD = spatial_ordering(st, layers[t], indexed=[left, middle])
+        prev_order = ORD[0:2]
+        total_order.append(ORD)
+
+    coords = {}
+    for t, layer in enumerate(total_order):
+        for x, v in enumerate(layer):
+            L = len(layer)
+            # coords[v] = [x - 1./2.*(t%2), sqrt(3)/2.*t]
+            coords[v] = [(x - .5 * (t % 2)) / L * 2 * np.pi, t / T * 2 * np.pi]
+    return coords
+    # print(spatial_ordering(st, layers[t], indexed=))
 
 
-def ordering(origin, layer):
-    boundary = origin
-    not_indexed = layer - {origin}
-    index = {origin: 0}
-    for i in range(len(layer)):
-        for f in not_indexed:
-            if (f & boundary).dim == 1:
-                not_indexed -= {f}
-                index[f] = index[boundary] + 1
-                boundary = f
-                break
-
-    return index
-
-
-def get_future(st: simplicial.Triangulation, face, layer):
-    for f in st.simplices[2]:
-        overlap = f & face
-        if overlap.dim == 1:
-            if meta[overlap]['s_type'] == (2, 0):
-                if get_t(f) == tuple(sorted([(i + 1) % st.time_size for i in get_t(face)])):
-                    return f
-    right = get_right(face, layer)
-    print(right)
-    return get_future(st, right, layer)
-
-
-def get_right(face, layer):
-    order = ordering(face, layer)
-    inv_map = {v: k for k, v in order.items()}
-    return inv_map[1]
-
-
-def alligned(l1, l2):
-    l1_up = {}
-    i = 0
-    for t in l1:
-        if meta[t]["s_type"] == (1, 2):
-            l1_up[i] = t
-            i += 1
-
-    l2_down = {}
-    i = 0
-    for t in l2:
-        if meta[t]["s_type"] == (2, 1):
-            l2_down[i] = t
-            i += 1
-    val = True
-    for i in range(len(l1_up)):
-        t1 = l1_up[i]
-        t2 = l2_down[i]
-        val = val and (t1 & t2).dim == 1
-    return val
-
-
-def valid_complete_ordering(layers):
-    T = len(layers)
-    val = True
-    for i in range(T):
-        val = val and alligned(layers[i], layers[(i + 1) % T])
-    return val
-
-
-
-
-layers = defaultdict(set)
-for tri in st.simplices[2]:
-    layers[get_t(tri)].add(tri)
-
-new_layers = []
-
-for layer in layers:
-
-
-ordering(list(layers[(0, 1)])[0], layers[(0, 1)])
-orig = list(layers[(0, 1)])[1]
-layer = layers[(0, 1)]
-print(orig)
-print(get_future(st, orig, layer))
-print()
-orig = list(layers[(0, 1)])[2]
-layer = layers[(0, 1)]
-print(orig)
-print(get_future(st, orig, layer))
+def nearest(ref, pnt):
+    deltas = np.array([[0, 1], [1, 0], [1, 1], [0, -1], [-1, 0], [-1, -1]]) * 2 * np.pi
+    sep = np.linalg.norm(pnt - ref)
+    final_delta = np.array([0, 0])
+    for delta in deltas:
+        test_sep = np.linalg.norm(pnt + delta - ref)
+        if test_sep < sep:
+            sep = test_sep
+            final_delta = delta
+    return pnt + final_delta
