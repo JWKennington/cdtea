ce160d0d1ee18af83843476c79ecbaed646b4950
Jackson Henry <jackson.henry3@gmail.com>
2021-06-24 13:16:26 -0400

    Split total ordering in to its own function

--- cdtea/Visualization/SpatialOrdering.py
+++ cdtea/Visualization/SpatialOrdering.py
@@ -1,11 +1,15 @@
 from cdtea.simplicial import Dim0SimplexKey, Triangulation
 
 
-def spatial_ordering(st: Triangulation, layer: list[Dim0SimplexKey], indexed: Dim0SimplexKey = None):
+def spatial_ordering(st: Triangulation, layer: list[Dim0SimplexKey], indexed: list[Dim0SimplexKey] = None):
     """
     returns an ordered array of nodes
 
     note that there are two valid orderings 01234 or 43210
+
+    layer: a list constaining all the verts with the same time index
+
+    indexed: if a first and second vertex are supplied then there is only a single valid ordering
     """
 
     # if origin is unspecified chose a random origin
@@ -16,10 +20,6 @@ def spatial_ordering(st: Triangulation, layer: list[Dim0SimplexKey], indexed: Di
         boundary = origin
     else:
         boundary = indexed[1]
-
-
-
-
 
     # all nodes that are not yet indexed
     not_indexed = set(layer) - set(indexed)
@@ -38,3 +38,40 @@ def spatial_ordering(st: Triangulation, layer: list[Dim0SimplexKey], indexed: Di
                 # once one has been found we can move on to the next loop
                 break
     return indexed
+
+
+def get_layer(st: Triangulation, t: int):
+    if 0 <= t < st.time_size:
+        return [n for n in st.nodes if st.simplex_meta[n]['t'] == t]
+    raise ValueError(f"t={t} must be between 0 and {st.time_size}")
+
+
+def total_ordering(st: Triangulation):
+    layers = {t: get_layer(st, t) for t in range(st.time_size)}
+
+    base_order = spatial_ordering(st, layers[0])
+
+    prev_orientation = base_order[0:2]
+    total_order = [base_order]
+    for t in range(1, st.time_size):
+        layer = layers[t]
+        past_left_vert, past_right_vert = prev_orientation[0:2]
+        for vert in layer:
+
+            left_overlap, right_overlap = vert | past_left_vert, vert | past_right_vert
+            connected_left, connected_right = left_overlap in st.edges, right_overlap in st.edges
+
+            if connected_left and connected_right:
+                middle = vert
+            elif connected_right:
+                right = vert
+            elif connected_left:
+                left = vert
+
+        if t % 2 == 0:
+            ORD = spatial_ordering(st, layers[t], indexed=[middle, right])
+        else:
+            ORD = spatial_ordering(st, layers[t], indexed=[left, middle])
+        prev_orientation = ORD[0:2]
+        total_order.append(ORD)
+    return total_order
