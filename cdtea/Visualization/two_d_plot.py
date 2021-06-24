412d7e43f729fba78d237d4c3808f41b8fa7b45c
Jackson Henry <jackson.henry3@gmail.com>
2021-06-24 10:04:10 -0400

    generate a 2d plot with a cut along a spatial and timelike direction, unfolding the st in to a square.

---
+++ cdtea/Visualization/two_d_plot.py
@@ -0,0 +1,61 @@
+from cdtea.Visualization.coordinates import toroidal_coordinates, nearest
+from cdtea.generate_flat import generate_flat_2d_space_time
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.collections import LineCollection
+from cdtea.simplicial import Triangulation
+import collections
+
+
+def two_d_plot(st: Triangulation):
+    ax = plt.gca()
+    meta = st.simplex_meta
+    coordinates = toroidal_coordinates(st)
+
+    # plot the points
+    X, Y, pnt_colors = [], [], []
+    edge_color = collections.defaultdict(int)
+    for e in st.simplices[1]:
+        for v in e:
+            edge_color[v] += 1
+    for v in coordinates:
+        X.append(coordinates[v][0])
+        Y.append(coordinates[v][1])
+        c_index = (edge_color[v] - 5.) / 7.
+        pnt_colors.append((c_index, 0, 1 - c_index, 1))
+
+    ax.scatter(X, Y, c=pnt_colors, s=100)
+
+    # plot all the edges
+    lines, line_colors = [], []
+    for e in st.simplices[1]:
+        b = e.basis_list
+        delta_t = abs(meta[b[0]]["t"] - meta[b[1]]["t"])
+        if delta_t == 0 or delta_t == 1:
+            pts = np.array([coordinates[v] for v in e])
+            pts = np.array([nearest(np.max(pts, 0), p) for p in pts])
+            center = np.mean(pts, 0)
+            pts = (pts - center) / 1.8 + center
+            lines.append(pts)
+            color = 'b'
+            if meta[e]['s_type'] == (2, 0):
+                color = 'r'
+            line_colors.append(color)
+    lc = LineCollection(lines, colors=line_colors)
+    ax.add_collection(lc)
+
+    # faces
+    for s in st.simplices[2]:
+        # check that this face isnt in the cut region
+        if all([all([abs(meta[v1]["t"] - meta[v2]["t"]) < 2 for v2 in s]) for v1 in s]):
+            pts = np.array([coordinates[v] for v in s])
+            pts = np.array([nearest(np.max(pts, 0), p) for p in pts])
+            center = np.mean(pts, 0)
+            pts = (pts - center) / 1.8 + center
+            color = (0, 0, 1, .5)
+            if meta[s]["s_type"] == (2, 1):
+                color = (1, 0, 0, .5)
+            p = Polygon(pts, closed=False, color=color)
+            ax.add_patch(p)
+    plt.show()
