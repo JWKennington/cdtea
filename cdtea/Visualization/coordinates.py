ad0fee307074907d4fdeaf3db8376244fbd18955
Jackson Henry <jackson.henry3@gmail.com>
2021-06-24 13:16:06 -0400

    Split total ordering in to its own function

--- cdtea/Visualization/coordinates.py
+++ cdtea/Visualization/coordinates.py
@@ -1,42 +1,23 @@
-from cdtea.Visualization.SpatialOrdering import spatial_ordering
+from cdtea.Visualization.SpatialOrdering import spatial_ordering, total_ordering
 from cdtea.simplicial import Triangulation
 import numpy as np
 
 
 def toroidal_coordinates(st: Triangulation):
-    meta = st.simplex_meta
-    nodes = st.simplices[0]
-    edges = st.simplices[1]
-    T = st.time_size
-    layers = {t: [n for n in nodes if meta[n]['t'] == t] for t in range(T)}
-    base_order = spatial_ordering(st, layers[0])
-    prev_order = base_order[0:2]
-    total_order = [base_order]
-    for t in range(1, T):
-        layer = layers[t]
-        L = len(layer)
-        for i in range(len(layer)):
-            if ((layer[i] | prev_order[0]) in edges) and ((layer[i] | prev_order[1]) in edges):
-                middle = layer[i]
-            elif (layer[i] | prev_order[1]) in edges:
-                right = layer[i]
-            elif (layer[i] | prev_order[0]) in edges:
-                left = layer[i]
-        if t % 2 == 0:
-            ORD = spatial_ordering(st, layers[t], indexed=[middle, right])
-        else:
-            ORD = spatial_ordering(st, layers[t], indexed=[left, middle])
-        prev_order = ORD[0:2]
-        total_order.append(ORD)
+    """
+    Generate a dict from node -> (theta,phi)
+
+    """
+    total_order = total_ordering(st)
 
     coords = {}
     for t, layer in enumerate(total_order):
+        L = len(layer)
         for x, v in enumerate(layer):
-            L = len(layer)
-            # coords[v] = [x - 1./2.*(t%2), sqrt(3)/2.*t]
-            coords[v] = [(x - .5 * (t % 2)) / L * 2 * np.pi, t / T * 2 * np.pi]
+            theta = (x - .5 * (t % 2)) / L * 2 * np.pi
+            phi = t / st.time_size * 2 * np.pi
+            coords[v] = (theta, phi)
     return coords
-    # print(spatial_ordering(st, layers[t], indexed=))
 
 
 def nearest(ref, pnt):
