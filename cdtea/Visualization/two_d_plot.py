b1d90ceba0aefc169fa76ead35b4ffec3593dc8b
Jackson Henry <jackson.henry3@gmail.com>
2021-08-22 17:38:01 -0400

    Switch to semantic simplex names. Add display flag.

--- cdtea/Visualization/two_d_plot.py
+++ cdtea/Visualization/two_d_plot.py
@@ -8,7 +8,7 @@ from cdtea.simplicial import Triangulation
 import collections
 
 
-def two_d_plot(st: Triangulation):
+def two_d_plot(st: Triangulation, display: bool = True):
     ax = plt.gca()
     meta = st.simplex_meta
     coordinates = toroidal_coordinates(st)
@@ -16,7 +16,7 @@ def two_d_plot(st: Triangulation):
     # plot the points
     X, Y, pnt_colors = [], [], []
     edge_color = collections.defaultdict(int)
-    for e in st.simplices[1]:
+    for e in st.edges:
         for v in e:
             edge_color[v] += 1
     for v in coordinates:
@@ -29,7 +29,7 @@ def two_d_plot(st: Triangulation):
 
     # plot all the edges
     lines, line_colors = [], []
-    for e in st.simplices[1]:
+    for e in st.edges:
         b = e.basis_list
         delta_t = abs(meta[b[0]]["t"] - meta[b[1]]["t"])
         if delta_t == 0 or delta_t == 1:
@@ -46,7 +46,7 @@ def two_d_plot(st: Triangulation):
     ax.add_collection(lc)
 
     # faces
-    for s in st.simplices[2]:
+    for s in st.faces:
         # check that this face isnt in the cut region
         if all([all([abs(meta[v1]["t"] - meta[v2]["t"]) < 2 for v2 in s]) for v1 in s]):
             pts = np.array([coordinates[v] for v in s])
@@ -58,4 +58,5 @@ def two_d_plot(st: Triangulation):
                 color = (1, 0, 0, .5)
             p = Polygon(pts, closed=False, color=color)
             ax.add_patch(p)
-    plt.show()
+    if display:
+        plt.show()
