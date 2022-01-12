77b200160a232769625b9f30c094933fc2ac1520
JWKennington <jameswkennington@gmail.com>
2022-01-12 17:05:56 -0500

    fix remove node and boundary logic

--- cdtea/moves.py
+++ cdtea/moves.py
@@ -27,6 +27,16 @@ def add_2D(trg: simplicial.Triangulation, edge: simplicial.SimplexKey):
     if trg.simplex_meta['t'][past_orig] > trg.simplex_meta['t'][fut_orig]:
         fut_orig, past_orig = past_orig, fut_orig
 
+    # TODO implement utility for time ordering nodes
+    fut_t, node_t, past_t = trg.simplex_meta['t'][fut_orig], trg.simplex_meta['t'][list(orig_nodes)[0]], trg.simplex_meta['t'][past_orig]
+
+    if abs(fut_t - node_t) == 1 and abs(node_t - past_t) == 1:  # Fully adjacent, no boundary overlap
+        if trg.simplex_meta['t'][past_orig] > trg.simplex_meta['t'][fut_orig]:
+            fut_orig, past_orig = past_orig, fut_orig
+    else:  # one node crosses boundary
+        if fut_t - node_t == -1 or node_t - past_t == -1:  # Not the correct ordering
+            fut_orig, past_orig = past_orig, fut_orig
+
     # Remove # TODO simplify this step to not require two loops
     to_remove = {edge, }
     for k in range(edge.dim + 1, 2 + 1):  # TODO add a 'dim' attribute to Triangulation
@@ -69,9 +79,19 @@ def rem_2D(trg: simplicial.Triangulation, node: simplicial.DimDSimplexKey):
     spacelike_edges = [e for e in edges if trg.simplex_meta['s_type'][e] == (2, 0)]
     timelike_edges = [e for e in edges if trg.simplex_meta['s_type'][e] == (1, 1)]
     spacelike_neighbors = trg.contains(spacelike_edges[0], dim=0).union(trg.contains(spacelike_edges[1], dim=0)).difference({node})
+
+
     fut_orig, past_orig = trg.contains(timelike_edges[0], dim=0).union(trg.contains(timelike_edges[1], dim=0)).difference({node})
-    if trg.simplex_meta['t'][past_orig] > trg.simplex_meta['t'][fut_orig]:
-        fut_orig, past_orig = past_orig, fut_orig
+
+    # TODO implement utility for time ordering nodes
+    fut_t, node_t, past_t = trg.simplex_meta['t'][fut_orig], trg.simplex_meta['t'][node], trg.simplex_meta['t'][past_orig]
+
+    if abs(fut_t - node_t) == 1 and abs(node_t - past_t) == 1: # Fully adjacent, no boundary overlap
+        if trg.simplex_meta['t'][past_orig] > trg.simplex_meta['t'][fut_orig]:
+            fut_orig, past_orig = past_orig, fut_orig
+    else: # one node crosses boundary
+        if fut_t - node_t == -1 or node_t - past_t == -1: # Not the correct ordering
+            fut_orig, past_orig = past_orig, fut_orig
 
     # Remove # TODO simplify this step to not require two loops
     to_remove = {node}
