377f27013bbdca4e43bdd71fb1b6c2ab58570558
JWKennington <jameswkennington@gmail.com>
2022-01-12 14:41:02 -0500

    simplify 2d_add face addition

--- cdtea/moves.py
+++ cdtea/moves.py
@@ -23,6 +23,9 @@ def add_2D(trg: simplicial.Triangulation, edge: simplicial.SimplexKey):
     nodes = trg.flatten(faces)
     orig_nodes = trg.contains(edge, dim=0)
     orig_layer = trg.simplex_meta['t'][list(orig_nodes)[0]]
+    fut_orig, past_orig = nodes.difference(orig_nodes)
+    if trg.simplex_meta['t'][past_orig] > trg.simplex_meta['t'][fut_orig]:
+        fut_orig, past_orig = past_orig, fut_orig
 
     # Remove # TODO simplify this step to not require two loops
     to_remove = {edge, }
@@ -33,26 +36,17 @@ def add_2D(trg: simplicial.Triangulation, edge: simplicial.SimplexKey):
         trg.remove_simplex(r)
 
     # Add
-    new_vertex = simplicial.Dim0SimplexKey(trg.max_index + 1)  # TODO test the new number
-    new_edges = ([(simplicial.DimDSimplexKey({new_vertex, old}), {'s_type': (2, 0)}) for old in orig_nodes] +
-                 [(simplicial.DimDSimplexKey({new_vertex, old}), {'s_type': (1, 1)}) for old in nodes.difference(orig_nodes)])
-    
-    new_triangles = []
-    for old1 in orig_nodes:
-        for old2 in nodes.difference(orig_nodes):
-            if time_sep(orig_layer, trg.simplex_meta['t'][old2], trg.time_size) > 0:
-                type = (2, 1)
-            elif time_sep(orig_layer, trg.simplex_meta['t'][old2], trg.time_size) < 0:
-                type = (1, 2)
-            new_triangles.append((simplicial.DimDSimplexKey({new_vertex, old1, old2}), {'s_type': type}))
+    new_simplices = []
+    new_simplices.append((simplicial.Dim0SimplexKey(trg.max_index + 1), {'t': orig_layer}))  # TODO test the new number
+    new_simplices = ([(simplicial.DimDSimplexKey({new_vertex, old}), {'s_type': (2, 0)}) for old in orig_nodes] +
+                     [(simplicial.DimDSimplexKey({new_vertex, old}), {'s_type': (1, 1)}) for old in nodes.difference(orig_nodes)])
 
-    trg.add_simplex(new_vertex, t=orig_layer)
+    new_simplices.extend([(simplicial.DimDSimplexKey({new_vertex, old, fut_orig}), {'s_type': (2, 1)}) for old in orig_nodes])
+    new_simplices.extend([(simplicial.DimDSimplexKey({new_vertex, old, past_orig}), {'s_type': (1, 2)}) for old in orig_nodes])
+
+    for new, kwargs in new_simplices:
+        trg.add_simplex(new, **kwargs)
+
     trg.simplex_meta['order'][new_vertex] = 4
-    for new, kwargs in new_edges:
-        trg.add_simplex(new, **kwargs)
-
-    for n in nodes.difference(orig_nodes):
-        trg.simplex_meta['order'][n] += 1
-
-    for new, kwargs in new_triangles:
-        trg.add_simplex(new, **kwargs)
+    trg.simplex_meta['order'][fut_orig] += 1
+    trg.simplex_meta['order'][past_orig] += 1
