d123c3ef8341e9281b48cdc256a39c5f1598fa17
Jackson Henry <jackson.henry3@gmail.com>
2022-01-31 12:15:00 -0500

    add documentation

--- cdtea/metropolis.py
+++ cdtea/metropolis.py
@@ -3,10 +3,21 @@ from cdtea import simplicial
 import numpy as np
 from cdtea.Visualization import two_d_plot
 from cdtea.tests import valid_triangulation
-np.random.seed(1)
+
+
+# np.random.seed(1)
 # pg 61,62
 
-def add_step(st: simplicial.Triangulation, lmbda: float):
+def add_step(st: simplicial.Triangulation, lmbda: float) -> bool:
+    """
+
+    Args:
+        st: the triangulation that is being operated on
+        lmbda: The effective cosmological constant
+
+    Returns: a bool representing weather or not the move was accepted
+
+    """
     N = st.num_nodes
 
     random_spatial_edge = np.random.choice(list(st.spatial_edges))
@@ -14,9 +25,20 @@ def add_step(st: simplicial.Triangulation, lmbda: float):
     acceptance_rate_add = min(1, N / (N + 1) * np.exp(-2 * lmbda))
     if acceptance_rate_add > np.random.random():
         add_2d(st, random_spatial_edge)
+        return True
+    return False
 
 
-def rem_step(st: simplicial.Triangulation, lmbda: float):
+def rem_step(st: simplicial.Triangulation, lmbda: float) -> bool:
+    """
+
+    Args:
+        st: the triangulation that is being operated on
+        lmbda: The effective cosmological constant
+
+    Returns: a bool representing weather or not the move was accepted
+
+    """
     N = st.num_nodes
     if len(st.rank_4_nodes) > 0:
         random_rank_4_node = np.random.choice(list(st.rank_4_nodes))
@@ -24,16 +46,26 @@ def rem_step(st: simplicial.Triangulation, lmbda: float):
         acceptance_rate_add = 1 / (min(1, N / (N + 1) * np.exp(-2 * lmbda)))
         if acceptance_rate_add > np.random.random():
             rem_2d(st, random_rank_4_node)
+            return True
+    return False
 
 
-def parity_step(st: simplicial.Triangulation, lmbda: float):
-    N = st.num_nodes
+def parity_step(st: simplicial.Triangulation) -> bool:
+    """
 
+    Args:
+        st: the triangulation that is being operated on
+
+    Returns: a bool representing weather or not the move was accepted
+
+    """
     random_mixed_face_temporal_edge = np.random.choice(list(st.mixed_face_temporal_edges))
 
     acceptance_rate_add = .5
     if acceptance_rate_add > np.random.random():
         parity_2d(st, random_mixed_face_temporal_edge)
+        return True
+    return False
 
 
 def step(st: simplicial.Triangulation, lmbda=np.log(2)):
@@ -46,19 +78,22 @@ def step(st: simplicial.Triangulation, lmbda=np.log(2)):
     Returns:
 
     """
-    add_step(st=st, lmbda=lmbda)
-    rem_step(st=st, lmbda=lmbda)
-    parity_step(st=st, lmbda=lmbda)
+    accepted_add = add_step(st=st, lmbda=lmbda)
+    accepted_rem = rem_step(st=st, lmbda=lmbda)
+    accepted_parity = parity_step(st=st)
+    return accepted_add, accepted_rem, accepted_parity
 
 
 import generate_flat
 
-st = generate_flat.generate_flat_2d_space_time(8, 8)
+st = generate_flat.generate_flat_2d_space_time(16, 16)
 
-for i in range(100):
+for i in range(500):
     print(i)
     step(st)
 import matplotlib.pyplot as plt
-valid_triangulation.is_valid(st)
+
+# valid_triangulation.is_valid(st)
+print(st.num_nodes)
 two_d_plot.two_d_plot(st)
 plt.show()
