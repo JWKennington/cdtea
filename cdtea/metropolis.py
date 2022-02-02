2ea64a55d13cfdd204504392cc4db1bed86c59a6
Jackson Henry <jackson.henry3@gmail.com>
2022-02-01 23:15:29 -0500

    remove extraneous script stuff

--- cdtea/metropolis.py
+++ cdtea/metropolis.py
@@ -1,11 +1,8 @@
 from cdtea.moves import add_2d, rem_2d, parity_2d
 from cdtea import simplicial
 import numpy as np
-from cdtea.Visualization import two_d_plot
-from cdtea.tests import valid_triangulation
 
 
-# np.random.seed(1)
 # pg 61,62
 
 def add_step(st: simplicial.Triangulation, lmbda: float) -> bool:
@@ -82,18 +79,3 @@ def step(st: simplicial.Triangulation, lmbda=np.log(2)):
     accepted_rem = rem_step(st=st, lmbda=lmbda)
     accepted_parity = parity_step(st=st)
     return accepted_add, accepted_rem, accepted_parity
-
-
-import generate_flat
-
-st = generate_flat.generate_flat_2d_space_time(16, 16)
-
-for i in range(500):
-    print(i)
-    step(st)
-import matplotlib.pyplot as plt
-
-# valid_triangulation.is_valid(st)
-print(st.num_nodes)
-two_d_plot.two_d_plot(st)
-plt.show()
