89626a2a36628a9cd0c1efaff973425b352d7852
Jackson Henry <jackson.henry3@gmail.com>
2022-05-08 00:22:02 -0400

    developing dilaton action

--- cdtea/Scripts/testing_observables.py
+++ cdtea/Scripts/testing_observables.py
@@ -27,18 +27,85 @@ def action_without_matter(st: simplicial.Triangulation):
         N += 1
     return -np.log(2) * N
 
-def dilaton_action(st:simplicial.Triangulation):
-    total_field = 0.
-    for n in st.faces:
-        total_field+=st.simplex_meta["dilaton"][n]
+
+def einstein_hilbert(st: simplicial.Triangulation):
+    order = st.simplex_meta['order']
+    tot = 0
+    Lambda = 0
+    ae = 1
+    for n in st.nodes:
+        # the deficit angle
+        epsilon = 2 * np.pi / 6 * (order[n] - 6)
+
+        tot += 1 / (8 * np.pi) * ae * (epsilon / ae - Lambda)
+    print(f"The total is {tot}")
     return -np.log(2) * st.num_nodes
 
 
+def dilaton(st: simplicial.Triangulation):
+    order = st.simplex_meta['order']
+    phi = st.simplex_meta['dilaton']
+    contains = st.simplex_meta['contains'].dual
+    tot = 0
+    Lambda = 0
+    ae = 1
+
+    def w(p):
+        return p
+
+    for f in st.faces:
+        # the average deficit angle
+        epsilon = 0
+        for n in f:
+            Tri_Number = simplicial.filter_simplices(contains[n], dim=2)
+
+            # The effective "deficit angle" for each face
+            # each node contributes angle inversely proportianal to the number of triangles its a part of
+            epsilon += (2 * np.pi / 6 * (order[n] - 6)) / len(Tri_Number)
+        epsilon = epsilon / 3
+        R = epsilon / ae
+
+        edges = simplicial.filter_simplices(st.simplex_meta['contains'][f], dim=1)
+        for edge in edges:
+            nodes = edge.basis_list
+            t = st.simplex_meta['t']
+            if t[nodes[0]] == t[nodes[1]]:
+                spatial_edge = edge
+                break
+
+        temporal_face = contains[spatial_edge]-{f,}
+        temporal_phi = phi[temporal_face.pop()]
+        d_phi_t = temporal_phi-phi[f]
+        """
+        There is a serious derivative problem here unless we define a global left and right parity.
+        without that the direction of a derivative isnt clear. 
+        """
+        d_phi_x = 0
+
+        tot += 1 / (8 * np.pi) * ae * (phi[f] * R - w(phi[f]) / phi[f] - Lambda)
+    print(f"The total is {tot}")
+    return -np.log(2) * st.num_nodes
+
+
+# def dilaton_action(st: simplicial.Triangulation):
+# total_action = 0.
+#
+# def v(phi):
+#     return phi
+# k = 1
+# phi = st.simplex_meta["dilaton"]
+# for n in st.faces:
+#     Delta_t_phi =
+#     delta_x_phi =
+#     (1/(2*k)*(phi*R-w(phi[n])/phi[n]*))
+#     total_action += v(phi)
+# return -np.log(2) * st.num_nodes
+
+
 # chain.run_chain(st, action, 1000, [], 1000, verbose=True)
-st, data = chain.run_chain(st, dilaton_action, 10000, [mes.volume_profile], 10, verbose=True)
+st, data = chain.run_chain(st, dilaton, 10000, [mes.volume_profile], 10, verbose=True)
 data = np.array(data).flatten()
 plt.hist(data)
 plt.show()
 two_d_plot.two_d_plot(st)
 plt.show()
-
